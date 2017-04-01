3043eca1fe71ca2679b9e0f540975803bd6550b6
moryta <raphael.moryta@gmail.com>
2017-04-01 16:48:44 -0300

    form idoso validacao de campo em branco

--- assets/idoso.js
+++ assets/idoso.js
@@ -2,14 +2,11 @@
  * Created by raphael on 01/04/17.
  */
 
-//sinaliza os campos em branco
-//formularioEnviar = 1 => envia a form
-$(document).ready(function () {
-
-    //Açao para o botao submit
-    $('#contatoSubmit').click(function(e){
+$(document).ready(function() {
+    $('form').on('submit', function(e){
+        // validation code here
+        e.preventDefault();
         var formularioEnviar = 1;
-        e.preventDefault(); // prevents default
         $('form input, form select, form textarea').each(
             function(index){
                 var input = $(this);
@@ -26,8 +23,46 @@ $(document).ready(function () {
                 }
             }
         );
-        //console.log(formularioEnviar);
+        if(formularioEnviar)
+        {
+
+        }
+        console.log(formularioEnviar);
+    });
+});
+
+
+
+/*
+$(document).ready(function () {
+
+    //Açao para o botao submit
+    $('#contatoSubmit').submit(function(e){
+        e.preventDefault(); // prevents default
+        var formularioEnviar = 1;
+        $('form input, form select, form textarea').each(
+            function(index){
+                var input = $(this);
+                $( this ).css({
+                    "background-color": ""
+                });
+                //alert('Type: ' + input.attr('type') + 'Name: ' + input.attr('id') + 'Value: ' + input.val() + 'Required' + input.attr('required'));
+                if(input.val() == '' && input.attr('required') == 'required')
+                {
+                    formularioEnviar = 0;
+                    $( this ).css({
+                        "background-color": "red"
+                    });
+                }
+            }
+        );
+        if(formularioEnviar)
+        {
+
+        }
+        console.log(formularioEnviar);
 
     });
 
 });
+*/
