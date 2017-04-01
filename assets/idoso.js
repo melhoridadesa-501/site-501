85dbb2dd0ceedf3ab56ff256e03cbd12158f3a81
moryta <raphael.moryta@gmail.com>
2017-04-01 15:53:11 -0300

    form idoso validacao de campo em branco

--- assets/idoso.js
+++ assets/idoso.js
@@ -1,12 +1,33 @@
 /**
  * Created by raphael on 01/04/17.
  */
+
+//sinaliza os campos em branco
+//formularioEnviar = 1 => envia a form
 $(document).ready(function () {
-/*
+
+    //Açao para o botao submit
     $('#contatoSubmit').click(function(e){
+        var formularioEnviar = 1;
         e.preventDefault(); // prevents default
-        alert('sdasds');
-        return false; // also prvents default (i like to add both but just the e.preventDefault is required :) )
+        $('form input, form select, form textarea').each(
+            function(index){
+                var input = $(this);
+                $( this ).css({
+                    "background-color": ""
+                });
+                //alert('Type: ' + input.attr('type') + 'Name: ' + input.attr('id') + 'Value: ' + input.val() + 'Required' + input.attr('required'));
+                if(input.val() == '' && input.attr('required') == 'required')
+                {
+                    formularioEnviar = 0;
+                    $( this ).css({
+                        "background-color": "red"
+                    });
+                }
+            }
+        );
+        //console.log(formularioEnviar);
+
     });
-    */
+
 });
